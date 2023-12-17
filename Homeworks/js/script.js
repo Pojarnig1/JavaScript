c36094247b2f60c74c8356746f4e8a0d9d2a8aeb
Pojarnig1 <andriets424@gmail.com>
2023-12-17 01:56:38 +0100

    123

--- Homeworks/js/script.js
+++ Homeworks/js/script.js
@@ -1,6 +1,6 @@
 //prompt
 //parseInt - для цілих чисел
 //parseFloat - для дійсних чисел
-let min = 10, max = 78;
-let a = min + Math.floor(Math.random() * (max - min + 1));
-console.log(a);
+let a = 'a'
+
+console.log(a.charCodeAt(0))
